55852cc3253870d9c48b76055990639dcd0aa63d
Buktahula <54060176+Buktahula@users.noreply.github.com>
2019-09-02 08:23:47 +0200

    Create config.js

---
+++ ccu2/config.js
@@ -0,0 +1,24 @@
+    
+  {
+  "name": "CCU2",
+  "version": "0.1",
+  "slug": "ccu2",
+  "description": "Homematic CCU2 for Homeassistant",
+  "url": "https://github.com/buktahula/hassio-addons/ccu2",
+  "webui": "http://[HOST]:[PORT:8080]",
+  "startup": "services",
+  "ingress": true,
+  "ingress_port": 8080,
+  "panel_icon": "mdi:console",
+  "panel_title": "Web",
+  "boot": "auto",
+  "options": {},
+  "schema": {},
+  "arch": [
+    "aarch64",
+    "amd64",
+    "armhf",
+    "armv7",
+    "i386"
+  ]
+  }
