d064b7e126695442f23252dc16ccaca765cf0b8a
Buktahula <54060176+Buktahula@users.noreply.github.com>
2019-09-02 10:45:40 +0200

    Create config.js

---
+++ pdftohtml/config.js
@@ -0,0 +1,23 @@
+  {
+  "name": "pdfviewer",
+  "version": "0.1",
+  "slug": "pdfviewer",
+  "description": "PDF-Viewer for Hassio",
+  "url": "https://github.com/alexanderriechel/hassio-addons/pdfviewer",
+  "webui": "http://[HOST]:[PORT:80]",
+  "startup": "services",
+  "ingress": true,
+  "ingress_port": 80,
+  "panel_icon": "mdi:console",
+  "panel_title": "Web",
+  "boot": "auto",
+  "options": {},
+  "schema": {},
+  "arch": [
+    "aarch64",
+    "amd64",
+    "armhf",
+    "armv7",
+    "i386"
+  ]
+  }
